0e1f921f58689911cea55bcddf24df7ba127ad68
pencilCool <yhtangcoder@gmail.com>
2019-05-21 07:33:20 +0800

    init

---
+++ TYHMainThreadBlock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TYHMainThreadBlock
+//
+//  Created by pencilCool on 2019/5/21.
+//  Copyright © 2019 pencilCool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TYHMainThreadBlockTests/TYHMainThreadBlockTests.swift
@@ -0,0 +1,34 @@
+//
+//  TYHMainThreadBlockTests.swift
+//  TYHMainThreadBlockTests
+//
+//  Created by pencilCool on 2019/5/21.
+//  Copyright © 2019 pencilCool. All rights reserved.
+//
+
+import XCTest
+@testable import TYHMainThreadBlock
+
+class TYHMainThreadBlockTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
