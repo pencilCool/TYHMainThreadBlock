a703f08464ec05f0b1a691f006d0aaf6409c15b7
pencilCool <yhtangcoder@gmail.com>
2019-05-21 08:23:31 +0800

    block main thread

---
+++ TYHMainThreadBlock/TYHMainThreadBlock.swift
@@ -0,0 +1,15 @@
+//
+//  YHTMainThreadBlock.swift
+//  TYHMainThreadBlock
+//
+//  Created by pencilCool on 2019/5/21.
+//  Copyright © 2019 pencilCool. All rights reserved.
+//
+
+import Foundation
+class TYHMainThreadBlock {
+    static let shared = TYHMainThreadBlock()
+    
+    
+    
+}
--- TYHMainThreadBlock/ViewController.swift
+++ TYHMainThreadBlock/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+//        let nib = UINib(nibName: "cell", bundle: nil)
+//        self.tableView.register(nib, forCellReuseIdentifier:"cell")
+//
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        doSomeHardTing()
+        return cell
+    }
+    
+    func doSomeHardTing() {
+        usleep(1000 * 100)
+    }
+    
 
 }
 
