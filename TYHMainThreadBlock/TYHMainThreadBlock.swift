3c52bcc4d463a1a5a34184db78538245db23d35f
pencilCool <yhtangcoder@gmail.com>
2019-05-23 00:20:04 +0800

    update

--- TYHMainThreadBlock/TYHMainThreadBlock.swift
+++ TYHMainThreadBlock/TYHMainThreadBlock.swift
@@ -7,10 +7,25 @@
 //
 
 import Foundation
+
+extension DispatchTime: ExpressibleByIntegerLiteral {
+    public init(integerLiteral value: Int) {
+        self = DispatchTime.now() + .seconds(value)
+    }
+}
+
+extension DispatchTime: ExpressibleByFloatLiteral {
+    public init(floatLiteral value: Double) {
+        self = DispatchTime.now() + .milliseconds(Int(value * 1000))
+    }
+}
+
+
 class TYHMainThreadBlock {
     static let shared = TYHMainThreadBlock()
     var semaphore:DispatchSemaphore!
     var runLoopObserver:CFRunLoopObserver!
+    var runLoopActivity:CFRunLoopActivity!
     
     func beginMonitor() {
         let unmanaged = Unmanaged.passRetained(self)
@@ -23,18 +38,36 @@ class TYHMainThreadBlock {
         context.info = vptr
 
         runLoopObserver = CFRunLoopObserverCreate(nil, CFRunLoopActivity.beforeTimers.rawValue, true, 0, {
-            (observer ,activety,info) in
+            (observer ,activity,context) in
+            let weakSelf = Unmanaged<TYHMainThreadBlock>.fromOpaque(context!).takeUnretainedValue()
+            weakSelf.runLoopActivity = activity
+            weakSelf.semaphore.signal()
             
-        }, &context)
+        },&context)
         
         CFRunLoopAddObserver(CFRunLoopGetMain(), runLoopObserver, CFRunLoopMode.commonModes)
-    }
-    
-    func callBackObserver() -> CFRunLoopObserverCallBack {
-        return {
-            (observer ,activiy,context) in
+        
+        DispatchQueue.global().async {
+            switch self.semaphore.wait(timeout: 3) {
+            case .timedOut:
+                guard let _ = self.runLoopObserver else {
+                 
+                    return
+                }
+                
+                if  self.runLoopActivity == .beforeSources ||
+                    self.runLoopActivity == .afterWaiting {
+                    print("monitor trigger")
+                }
+
+                print("timeout")
+            case .success:
+                print("success")
+            }
             
         }
     }
-    
+
+
+
 }
